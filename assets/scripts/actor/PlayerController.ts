e6410d959df37b62c9a2ec5af2f2c4b0cec0480a
DESKTOP-9F9TBRE\whb <whbzealot@qq.com>
2022-12-04 14:31:26 +0800

    + base

---
+++ assets/scripts/actor/PlayerController.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('Player')
+export class Player extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
---
+++ assets/scripts/level/TriggerMapEvent.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('MapEvent')
+export class MapEvent extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
